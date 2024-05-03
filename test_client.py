99234bf92aa982a2e63d563035eea3501a484c45
cameronpool <cameronpool2019@gmail.com>
2024-05-03 08:55:25 -0500

    :)

--- test_client.py
+++ test_client.py
@@ -6,7 +6,7 @@ class TestConeApp(unittest.TestCase):
     def test_add_cone(self):
         cone_data = {"radius": 5, "height": 10, "slantHeight": 12}
         r = requests.post("http://localhost:3000/addCone", json=cone_data)
-        self.assertEqual(r.status_code, 201)  # Assuming 201 is the status code for successful cone addition
+        self.assertEqual(r.status_code, 201)  # 201 = cone adding successfully
         data = r.json()
         self.assertIn("coneID", data)  # Check if coneID is present in the response
 
@@ -14,11 +14,11 @@ class TestConeApp(unittest.TestCase):
         coneID = "20474c7c-7f60-4b5c-b4d6-4193727114a6"
         updated_cone_data = {"radius": 6, "height": 12, "slantHeight": 14}
         r = requests.put(f"http://localhost:3000/updateCone/{coneID}", json=updated_cone_data)
-        self.assertEqual(r.status_code, 200)  # Assuming 200 is the status code for successful cone update
+        self.assertEqual(r.status_code, 200)  # 200 = successful cone update
 
     def test_display_all_cones(self):
         r = requests.get("http://localhost:3000/getAllCones")
-        self.assertEqual(r.status_code, 200)  # Assuming 200 is the status code for successful request
+        self.assertEqual(r.status_code, 200)  # 200 = successful request
 
 if __name__ == '__main__':
     unittest.main()
